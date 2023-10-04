46f9fa8163bcd5c7de8f093ff5908ffcff07fb84
Ryan <49217984+sRemorse@users.noreply.github.com>
2023-10-04 19:05:02 +0100

    Moved cooldown handler - Removed cooldown handlers from ping and serverinfo commands + Added global cooldown handler in main

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -1,6 +1,5 @@
 import disnake
 from disnake.ext import commands
-import embeds
 
 class ServerinfoCommand(commands.Cog):
     """Server info command"""
@@ -13,12 +12,7 @@ class ServerinfoCommand(commands.Cog):
     async def serverinfo(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.send_message(
         f'Server name: {inter.guild.name}\nTotal members: {inter.guild.member_count}\nCreated on: {inter.guild.created_at.strftime("%d-%m-%Y")}'
-    )
-        
-    @serverinfo.error
-    async def cooldown_error(self, inter, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await inter.response.send_message(embed=embeds.createCooldownEmbed(round(error.retry_after)), ephemeral=True, delete_after=error.retry_after)
-
+    )           
+            
 def setup(bot: commands.Bot):
     bot.add_cog(ServerinfoCommand(bot))
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import disnake
 import os
+import embeds
 
 from dotenv import load_dotenv
 from disnake.ext import commands
 
 load_dotenv()
 
-bot = commands.InteractionBot(test_guilds=[1158529104384098426])
+bot = commands.InteractionBot(
+    test_guilds=[1158529104384098426],
+    activity=disnake.Game(name=os.getenv("STATUS"))
+    )
 
 @bot.event
 async def on_ready():
     print("The bot is ready!")
     print(f"Logged in as {bot.user} in {len(bot.guilds)} guilds")
+    print(f"Status: {bot.status}")
+    print(f"Activity: {bot.activity}")
+
+# Global cooldown handler
+@bot.event
+async def on_slash_command_error(inter, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await inter.response.send_message(embed=embeds.createCooldownEmbed(round(error.retry_after)), ephemeral=True, delete_after=error.retry_after)
 
 bot.load_extensions("cogs/")
 bot.run(os.getenv("DISCORD_TOKEN"))
